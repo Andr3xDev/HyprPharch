3cc7460b5540d254ccd8e636e0c658374e557569
Andr3xDev <achavarropp@gmail.com>
2025-01-17 00:00:25 -0500

    Add LSP

--- dots/nvim/lua/plugins/code/lspconfig.lua
+++ dots/nvim/lua/plugins/code/lspconfig.lua
@@ -1,3 +1,101 @@
-return {
 
+return {
+    -- Mason: Instalador de servidores, linters y formateadores
+    {
+        "williamboman/mason.nvim",
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    -- Mason-LSPconfig: Integración automática de Mason con LSPconfig
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "jdtls",      -- Java
+                    "cssls",      -- CSS
+                    "tsserver",   -- JavaScript/TypeScript
+                    "pyright",    -- Python
+                    "sqlls",      -- SQL
+                    "html",       -- HTML
+                    "marksman",   -- Markdown
+                    "lua_ls",     -- Lua
+                    "bashls"      -- Bash
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+    -- LSPconfig: Configuración de servidores LSP
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "williamboman/mason-lspconfig.nvim" },
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            local on_attach = function(_, bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            end
+
+            local servers = {
+                lua_ls = {         -- Lua
+                    settings = {
+                        Lua = {
+                            runtime = { version = "LuaJIT" },
+                            diagnostics = { globals = { "vim" } },
+                            workspace = {
+                                library = vim.api.nvim_get_runtime_file("", true),
+                            },
+                            telemetry = { enable = false },
+                        },
+                    },
+                },
+            }
+
+            for server, config in pairs(servers) do
+                lspconfig[server].setup(vim.tbl_extend("force", {
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                }, config))
+            end
+        end,
+    },
+    -- nvim-cmp: Motor de autocompletado
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip", -- Snippets
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end,
+    },
 }
--- dots/nvim/lua/plugins/ui/lualine.lua
+++ dots/nvim/lua/plugins/ui/lualine.lua
@@ -53,7 +53,24 @@ return {
 					lualine_b = {},
 					lualine_c = {},
 					lualine_x = {},
-					lualine_y = {},
+					lualine_y = {
+                        function()
+                            local msg = 'No Active Lsp'
+                            local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+                            local clients = vim.lsp.get_clients()
+                            if next(clients) == nil then
+                                return msg
+                            end
+                            for _, client in ipairs(clients) do
+                                local filetypes = client.config.filetypes
+                                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                                    return client.name
+                                end
+                            end
+                        return msg
+                    end,
+                    icon = ' LSP:',
+                },
 					lualine_z = { 'fileformat' }
 				},
 				tabline = {},
