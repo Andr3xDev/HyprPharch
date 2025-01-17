17ea2e354e6287956c946eed2ba190872fdeaac1
Andr3xDev <achavarropp@gmail.com>
2025-01-17 00:02:39 -0500

    Remove LSP inactive

--- dots/nvim/lua/plugins/ui/lualine.lua
+++ dots/nvim/lua/plugins/ui/lualine.lua
@@ -53,26 +53,9 @@ return {
 					lualine_b = {},
 					lualine_c = {},
 					lualine_x = {},
-					lualine_y = {
-                        function()
-                            local msg = 'No Active Lsp'
-                            local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
-                            local clients = vim.lsp.get_clients()
-                            if next(clients) == nil then
-                                return msg
-                            end
-                            for _, client in ipairs(clients) do
-                                local filetypes = client.config.filetypes
-                                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                                    return client.name
-                                end
-                            end
-                        return msg
-                    end,
-                    icon = ' LSP:',
+					lualine_y = {},
+					lualine_z = { 'fileformat' },
                 },
-					lualine_z = { 'fileformat' }
-				},
 				tabline = {},
 				winbar = {},
 				inactive_winbar = {},
