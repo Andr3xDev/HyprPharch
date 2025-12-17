7110373bb21e616456de629252d575ecd2548478
Andr3xDev <andr3xdev@gmail.com>
2025-12-17 16:16:00 -0500

    fix: border color in rose pine dawn

---
+++ rofi/theme.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ~/.config/rofi
+
+case "$1" in
+    gruvbox-material-d)
+        ln -sf gruvbox-material-d.rasi colors.rasi
+        ;;
+    gruvbox-material-l)
+        ln -sf gruvbox-material-l.rasi colors.rasi
+        ;;
+    rose-pine-l)
+        ln -sf rose-pine-l.rasi colors.rasi
+        ;;
+    rose-pine-d)
+        ln -sf rose-pine-d.rasi colors.rasi
+        ;;
+    *)
+        echo "Usage: rofi-theme {gruvbox|rose}"
+        exit 1
+        ;;
+esac
