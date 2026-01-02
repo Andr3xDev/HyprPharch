b6a04c8715d17fe9bc0c6223e6af104ee04da86b
Andr3xDev <andr3xdev@gmail.com>
2026-01-02 01:56:53 -0500

    update: window rules

--- config/hypr/scripts/spotify-theme.sh
+++ config/hypr/scripts/spotify-theme.sh
@@ -5,18 +5,22 @@ THEME="$1"
 case "$THEME" in
     "gruvbox-material-d")
         COLOR_SCHEME="gruvbox-material-dark"
+        THEME_SPOTIFY="Dribbblish"
         ;;
         
     "gruvbox-material-l")
-        COLOR_SCHEME="catppuccin-latte"
+        COLOR_SCHEME="light"
+        THEME_SPOTIFY="Onepunch"
         ;;
         
     "rose-pine-d")
         COLOR_SCHEME="rosepine"
+        THEME_SPOTIFY="Dribbblish"
         ;;
         
     "rose-pine-l")
         COLOR_SCHEME="white"
+        THEME_SPOTIFY="Dribbblish"
         ;;
         
     *)
@@ -27,7 +31,7 @@ esac
 
 killall spotify 2>/dev/null
 
-spicetify config current_theme Dribbblish color_scheme "$COLOR_SCHEME"
+spicetify config current_theme "$THEME_SPOTIFY" color_scheme "$COLOR_SCHEME"
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
 spicetify config sidebar_config 0
 spicetify apply
